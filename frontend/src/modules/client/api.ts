ef2f49c32b65a722b34c241ddb5099141b9c9a68
Bogdan Ziorkiewicz <bogdan.ziorkiewicz@schibsted.com>
2021-05-27 13:25:31 +0200

    Frontend - basic API

--- frontend/src/modules/client/api.ts
+++ frontend/src/modules/client/api.ts
@@ -1,41 +1,35 @@
-import { Item, Status } from './types';
+import * as superagent from 'superagent'
+
+import { Item } from './types';
 
 
-const items = [
-    {
-        id: 1,
-        name: 'Google VR oculus 1500',
-        status: Status.UNAVAILABLE,
-        rentedBy: {
-            id: 1,
-            name: 'Piotr Nęcka',
-        },
-    },
-    {
-        id: 2,
-        name: 'Solniczka',
-        status: Status.AVAILABLE,
-    },
-    {
-        id: 3,
-        name: 'Dron Mavic Air 2',
-        status: Status.UNAVAILABLE,
-        rentedBy: {
-            id: 2,
-            name: 'Rafał Gasiński',
-        },
-    },
-    {
-        id: 3,
-        name: 'Lodówko zamrażarka BEKA xD',
-        status: Status.AVAILABLE,
-    },
-];
+const agent = superagent.agent();
+
+agent.use((request) => {
+    request.url = `http://34.89.235.4/api/${request.url}`;
+});
+
+interface RequestOptions {
+    method?: 'GET'|'POST'|'PUT'|'PATCH'|'DELETE';
+}
+
+export function request<T = any> (urlOrPath: string, payload?: Object, options?: RequestOptions): Promise<T> {
+    const method = options?.method || (payload ? 'POST' : 'GET');
+    const agentMethod: any = method.toLowerCase();
+    // @ts-ignore
+    const promise = payload ? agent[agentMethod](urlOrPath).send(payload) : agent[agentMethod](urlOrPath);
+
+    return promise.then((response: Response) => {
+        return response?.body;
+    }).catch((error: string) => {
+        throw new Error(error);
+    });
+}
 
 export function getItems (): Promise<Item[]> {
-    return Promise.resolve(items);
+    return request('items');
 }
 
 export function getItem (itemId: number): Promise<Item> {
-    return Promise.resolve(items.find(item => item.id === itemId) || items[0]);
+    return request(`items/${itemId}`)
 }

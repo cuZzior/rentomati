facf18eab29552b2eeef341358c4cade4a4c0fc7
cuZzior <piotr.necka@gmail.com>
2021-05-27 11:20:40 +0200

    fix evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,25 +10,25 @@ CREATE TABLE rentomati.item
 
 CREATE TABLE rentomati.user
 (
-    id          INT UNSIGNED PRIMARY KEY NOT NULL,
-    name        VARCHAR(256) NOT NULL
+    id   INT UNSIGNED PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE rentomati.reservation
 (
-    id          INT UNSIGNED PRIMARY KEY NOT NULL,
-    user_id     INT UNSIGNED NOT NULL,
-    item_id     INT UNSIGNED NOT NULL,
-    start_date  TEXT         NOT NULL,
+    id         INT UNSIGNED PRIMARY KEY NOT NULL,
+    user_id    INT UNSIGNED NOT NULL,
+    item_id    INT UNSIGNED NOT NULL,
+    start_date TEXT NOT NULL,
     CONSTRAINT fk_reservation_user FOREIGN KEY (user_id)
-        REFERENCES rentomati.user (name)
+        REFERENCES rentomati.user (id)
         ON DELETE CASCADE,
     CONSTRAINT fk_reservation_item FOREIGN KEY (item_id)
         REFERENCES rentomati.item (id)
         ON DELETE CASCADE
 );
 
--- !Down
+-- !Downs
 DROP TABLE rentomati.reservation;
 DROP TABLE rentomati.item;
 DROP TABLE rentomati.user;
